181c2cc56554e7ce8d0eaed7d6f04a942f3cffb7
VAM Design <vicki@vamdesign.com>
2025-03-25 17:56:39 -0700

    Fixed paths by removing beta references and updated build files

--- next.config.js
+++ next.config.js
@@ -4,8 +4,7 @@ const nextConfig = {
   // Only use static export for production builds
   ...(process.env.NODE_ENV === 'production' && {
     output: 'export',
-    basePath: '/beta',
-    assetPrefix: '/beta/',
+    
   }),
   images: {
     unoptimized: true,
@@ -17,7 +16,7 @@ const nextConfig = {
         {
           loader: 'file-loader',
           options: {
-            publicPath: '/beta',
+            publicPath: '/',
             name: 'static/media/[name].[hash].[ext]'
           }
         }
