d9a73c764f75b8eb297ea139846218255120615d
VAM Design <vicki@vamdesign.com>
2025-08-23 08:47:38 -0700

    Create page.tsx
    testing

---
+++ app/test-clara/page.tsx
@@ -0,0 +1,9 @@
+export default function TestClaraPage() {
+  return (
+    <div style={{ padding: '50px', textAlign: 'center' }}>
+      <h1>TEST PAGE - Clara Test</h1>
+      <p>If you can see this, the deployment works!</p>
+      <p>This means the issue is with the original Clara page code.</p>
+    </div>
+  )
+}
