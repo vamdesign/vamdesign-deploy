8133fe6da4e9e18c1e09be137f960a81d0461ed0
vamdesign <vicki@vamdesign.com>
2026-01-26 22:59:39 +0000

    Deploying to docs from @ vamdesign/vamdesign-deploy@59e2243410ab5656e3ed44996ca045f07cca885e 🚀

---
+++ _next/static/chunks/app/about/page-611cc62eefc77506.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[220],{3573:(e,s,n)=>{Promise.resolve().then(n.bind(n,7067)),Promise.resolve().then(n.t.bind(n,1356,23))}},e=>{e.O(0,[901,356,67,441,255,358],()=>e(e.s=3573)),_N_E=e.O()}]);
