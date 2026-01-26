637bda88d32ede5b52801e6a724ef419c085e91b
VAM Design <vicki@vamdesign.com>
2026-01-26 15:12:39 -0800

    Update footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="py-8 px-4 mt-4 text-center">
       <p className="font-montserrat text-[0.65rem] text-[#5f5f5f]/80">
-        Made with the help of my AI friends. VAM Design. All rights reserved {new Date().getFullYear()}.
+       VAM Design. All rights reserved {new Date().getFullYear()}.
       </p>
     </footer>
   )
